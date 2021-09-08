7f5815e45dea45642c41fcea05c64529dafd8b97
alexojo <75278388+alexojo@users.noreply.github.com>
2021-09-07 19:11:33 -0500

    Update Program.cs

--- SimpleCalc/Program.cs
+++ SimpleCalc/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-// Principal Program xd
+// Principal Program jaja
 namespace SimpleCalc
 {
     public class Program
